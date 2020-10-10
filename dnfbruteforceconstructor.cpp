a0da11d4d13c6dfcb8b25fc6f5278610251de0ec
Dmitry Monakhov <dimamon6032403@gmail.com>
2020-10-10 22:12:03 +0300

    Bruteforces made parallel

--- dnfbruteforceconstructor.cpp
+++ dnfbruteforceconstructor.cpp
@@ -1,9 +1,145 @@
 #include "dnfbruteforceconstructor.h"
 #include "setutils.h"
+#include <thread>
+
+std::mutex m_mutex;
+
 
 DnfBruteforceConstructor::DnfBruteforceConstructor()
 {
 
+}
+
+void DnfBruteforceConstructor::ProcessVariations(LogicEquation *result, LogicEquation* sourceEquation, LogicEquationNode **outOperand, int maskStart, int maskEnd)
+{
+    int eventsCount = sourceEquation->getEventsCount();
+    bool *input = new bool[eventsCount];
+    LogicEquationNode *prevConjunction = nullptr;
+
+    for(int mask = maskStart; mask <= maskEnd; mask++)
+    {
+        for(int i = 0; i < eventsCount; i++)
+        {
+            input[i] = mask & (1 << i);
+        }        
+
+        bool combinationResult = sourceEquation->Resolve(input, eventsCount);
+        if(combinationResult == true)
+        {
+            LogicEquationNode *prevDisjunction = nullptr;
+            for(int i = 0; i < eventsCount; i++)
+            {
+                LogicEquationNode *disjunction;
+                if(input[i])
+                {
+                    std::lock_guard<std::mutex> lock(m_mutex);
+                    disjunction = result->AddUniquePrimitive(i);
+                }
+                else
+                {
+                    std::lock_guard<std::mutex> lock(m_mutex);
+                    LogicEquationNode *negatedEvent = result->AddUniquePrimitive(i);
+                    disjunction = new LogicEquationNode(LogicOperation::Negation, negatedEvent, nullptr);
+                    result->AddEquationNode(disjunction);
+                }
+
+                if(prevDisjunction == nullptr)
+                {
+                    prevDisjunction = disjunction;
+                }
+                else
+                {
+
+                    prevDisjunction = new LogicEquationNode(LogicOperation::Disjunction, prevDisjunction, disjunction);
+                    std::lock_guard<std::mutex> lock(m_mutex);
+                    result->AddEquationNode(prevDisjunction);
+                }
+            }
+
+            if(prevConjunction == nullptr)
+            {
+                prevConjunction = prevDisjunction;
+            }
+            else
+            {
+                prevConjunction = new LogicEquationNode(LogicOperation::Conjunction, prevConjunction, prevDisjunction);
+                std::lock_guard<std::mutex> lock(m_mutex);
+                result->AddEquationNode(prevConjunction);
+            }
+        }
+    }
+
+    *outOperand = prevConjunction;
+
+    delete[] input;
+}
+
+LogicEquation *DnfBruteforceConstructor::GetPerfectDisjunctiveNormalForm(LogicEquation* sourceEquation, int threads_count)
+{
+    LogicEquation *result = new LogicEquation();
+
+    int eventsCount = sourceEquation->getEventsCount();
+
+    int variationsCount = 1 << eventsCount; // 2 ^ eventsCount;
+
+    int variationsPerThread = variationsCount / threads_count;
+
+    QVector<std::thread*> threadList = QVector<std::thread*>();
+    QVector<LogicEquationNode**> operandsList = QVector<LogicEquationNode**>();
+
+    int variationsLeft = variationsCount;
+    for(int i = 0; i < threads_count; i++)
+    {
+        int maskStart = variationsPerThread * i;
+        int maskEnd = maskStart + variationsPerThread;
+        variationsLeft -= variationsPerThread;
+        LogicEquationNode **outOperand = new LogicEquationNode*;
+        operandsList.append(outOperand);
+        std::thread *thr = new std::thread(ProcessVariations, result, sourceEquation, outOperand, maskStart, maskEnd);
+        threadList.append(thr);
+    }
+
+    if(variationsLeft > 0)
+    {
+        int maskEnd = variationsCount;
+        int maskStart = variationsCount - variationsLeft;
+        LogicEquationNode **outOperand = new LogicEquationNode*;
+        operandsList.append(outOperand);
+        std::thread *thr = new std::thread(ProcessVariations, result, sourceEquation, outOperand, maskStart, maskEnd);
+        threadList.append(thr);
+    }
+
+    for(int i = 0; i < threadList.length(); i++)
+    {
+        std::thread *thr = threadList.at(i);
+        thr->join();
+        delete thr;
+    }
+
+    LogicEquationNode *prevOperand = nullptr;
+    for(int i = 0; i < operandsList.length(); i++)
+    {
+        LogicEquationNode **operandPointer = operandsList[i];
+        LogicEquationNode *operand = *operandPointer;
+        delete operandPointer;
+        if(operand == nullptr)
+        {
+            continue;
+        }
+        if(prevOperand == nullptr)
+        {
+            prevOperand = operand;
+        }
+        else
+        {
+            LogicEquationNode *conjunction = new LogicEquationNode(LogicOperation::Conjunction, prevOperand, operand);
+            result->AddEquationNode(conjunction);
+            prevOperand = conjunction;
+        }
+    }
+
+    result->SetRootNode(prevOperand);
+    return result;
 }
 
 LogicEquation *DnfBruteforceConstructor::GetPerfectDisjunctiveNormalForm(LogicEquation* sourceEquation)
@@ -130,7 +266,12 @@ LogicEquation* DnfBruteforceConstructor::GetPdnf(QVector<Path<NetParams>*> *path
 
     LogicEquation *dnf = new LogicEquation();
     Construct(dnf, &listClone);
+
+#if USE_PARALLEL == 0
     LogicEquation *pdnf = GetPerfectDisjunctiveNormalForm(dnf);
+#else
+    LogicEquation *pdnf = GetPerfectDisjunctiveNormalForm(dnf, USE_THREADS);
+#endif
     delete dnf;
 
     return pdnf;
--- dnfbruteforceconstructor.h
+++ dnfbruteforceconstructor.h
@@ -4,6 +4,10 @@
 #include "logicequation.h"
 #include "netparams.h"
 #include "path.h"
+#include <mutex>
+
+#define USE_PARALLEL    1
+#define USE_THREADS     32
 
 
 
@@ -13,8 +17,13 @@ class DnfBruteforceConstructor
 public:
 
     static LogicEquation *GetPdnf(QVector<Path<NetParams>*> *pathList);
+
 private:
     static LogicEquation *GetPerfectDisjunctiveNormalForm(LogicEquation *sourceEquation);
+//#if USE_PARALLEL == 1
+    static void ProcessVariations(LogicEquation *result, LogicEquation* sourceEquation, LogicEquationNode **outOperand, int maskStart, int maskEnd);
+    static LogicEquation *GetPerfectDisjunctiveNormalForm(LogicEquation* sourceEquation, int threads_count);
+//#endif
     static LogicEquationNode *ConstructMultiplicative(LogicEquation *equation, Path<NetParams> *path);
     static void Construct(LogicEquation *equation, QVector<Path<NetParams> *> *pathList);
 };
